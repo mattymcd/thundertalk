85d4af7cbd6609c2dd3c5d6b9a58c1f6a9332cc4
mattymcd <matt.mcdole@gmail.com>
2015-04-14 13:35:28 -0700

    add notes

--- basic auth/blog/app/models/user.rb
+++ basic auth/blog/app/models/user.rb
@@ -15,6 +15,15 @@ class User < ActiveRecord::Base
     else
       nil
     end
+
+# What this does
+# Given an email and password
+# Lookup a user in the database
+# If a user is found
+# Hash the password with that user’s salt using the same bcrypt method we used originally to hash the user’s password
+# If the hashed password matches with what is stored in the database, return the user’s object
+# If it does not match, return nil
+
   end
 
 end
